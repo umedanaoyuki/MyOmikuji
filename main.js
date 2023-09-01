64344d652fa1bb8409eb8ff518ba431ac2e59cff
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-01 23:47:36 +0900

    Math.floor, math.random

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
     const result = document.getElementById('result');
 
     btn.addEventListener('click', () => {
-        result.textContent = 'hit!';
+        const n = Math.floor(Math.random()*3);
+        result.textContent = n;
     });
 }
