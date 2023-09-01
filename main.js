86fd6c7575ec1cd60f46e51eb5887db191be8b44
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-01 23:43:05 +0900

    ボタンイベントの追加

---
+++ main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+{
+    const btn = document.getElementById('btn');
+    const result = document.getElementById('result');
+
+    btn.addEventListener('click', () => {
+        result.textContent = 'hit!';
+    });
+}
